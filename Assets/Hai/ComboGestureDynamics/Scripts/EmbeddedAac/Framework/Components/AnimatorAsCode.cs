aafa98458bcb1c4d56fccddc737317553ccb3583
Haï~ <hai-vr@users.noreply.github.com>
2021-12-31 01:32:42 +0100

    Add embedded Animator As Code: - Add embedded Animator As Code

---
+++ Assets/Hai/ComboGestureDynamics/Scripts/EmbeddedAac/Framework/Components/AnimatorAsCode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using VRC.SDK3.Avatars.Components;
+
+namespace Hai.ComboGestureDynamics.Scripts.EmbeddedAac.Framework.Components
+{
+    public abstract class AnimatorAsCode : MonoBehaviour
+    {
+        public VRCAvatarDescriptor avatar;
+        public RuntimeAnimatorController assetHolder;
+        public string layerNameSuffix;
+        public string parameterName;
+
+        public AacInternal _internal;
+    }
+
+    [Serializable]
+    public struct AacInternal
+    {
+        public bool created;
+        public string assetKey;
+        public int createdWithMajorVersion;
+    }
+}
---
+++ Assets/Hai/ComboGestureDynamics/Scripts/EmbeddedAac/Framework/Editor/EditorUI/AnimatorAsCodeEditor.cs
@@ -0,0 +1,48 @@
+﻿using Hai.Cge2Aac.Framework.Editor.Internal.V0;
+using Hai.ComboGestureDynamics.Scripts.EmbeddedAac.Framework.Components;
+using UnityEditor;
+using UnityEngine;
+
+namespace Hai.ComboGestureDynamics.Scripts.EmbeddedAac.Framework.Editor.EditorUI
+{
+    [CustomEditor(typeof(AnimatorAsCode), true)]
+    [CanEditMultipleObjects]
+    public class AnimatorAsCodeEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            if (!serializedObject.isEditingMultipleObjects)
+            {
+                var current = (AnimatorAsCode)target;
+                // EditorGUILayout.HelpBox(current.Describe(), MessageType.None);
+
+                DrawDefaultInspector();
+
+                if (GUILayout.Button("Create"))
+                {
+                    AacRegistry.Registry.Create(current);
+                }
+                EditorGUI.BeginDisabledGroup(!current._internal.created);
+                if (GUILayout.Button("Remove"))
+                {
+                    AacRegistry.Registry.Remove(current);
+                }
+                EditorGUI.EndDisabledGroup();
+
+                EditorGUILayout.TextField("Asset key (read only)", current._internal.assetKey);
+            }
+            else
+            {
+                DrawDefaultInspector();
+
+                if (GUILayout.Button("Create all"))
+                {
+                    foreach (var t in targets)
+                    {
+                        AacRegistry.Registry.Create((AnimatorAsCode)t);
+                    }
+                }
+            }
+        }
+    }
+}
