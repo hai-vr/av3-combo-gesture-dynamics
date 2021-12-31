31ae8f133f509ab0d41c43fcc3cb7b421fbfcd2c
Haï~ <hai-vr@users.noreply.github.com>
2021-12-31 01:36:37 +0100

    Add Expressions Library and Expressions Layer: - Expressions Library contains all animations as a 1D blend tree - Two instances of Expressions Library are controlled by 2 different parameters acting an animation selectors - The two libraries are interpolated as a 1D blend tree controlled by a tweening parameter

---
+++ Assets/Hai/ComboGestureDynamics/Scripts/Editor/CgdExpressionLibrary.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Hai.ComboGestureDynamics.Scripts.EmbeddedAac.Framework.Components;
+using Hai.ComboGestureDynamics.Scripts.EmbeddedAac.Framework.Editor.Internal.V0;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace Hai.ComboGestureDynamics.Scripts.Editor
+{
+    public class CgdController : AnimatorAsCode
+    {
+    }
+
+    public class CgdExpressionLibrary
+    {
+        private readonly List<Motion> motions;
+        private readonly AacV0.AacFlBase<CgdController> _aac;
+        private readonly CgdParameters parameters;
+
+        public CgdExpressionLibrary(List<Motion> motions, CgdParameters parameters)
+        {
+            this.motions = motions.ToList();
+            this.parameters = parameters;
+            _aac = AacV0.Using(new CgdController());
+        }
+
+        public void Generate()
+        {
+            _aac.ResetAssetHolder();
+
+            GenerateExpressionsLayer();
+        }
+
+        private void GenerateExpressionsLayer()
+        {
+            var expressionsLayer = _aac.CreateSupportingFxLayer("Expressions");
+            var tweeningTree = CreateTweeningTree();
+            expressionsLayer.NewState("Expressions", 0, 0).WithAnimation(tweeningTree);
+        }
+
+        private BlendTree CreateTweeningTree()
+        {
+            var expLibForA = CreateExpressionLibraryBlendTree(parameters.ExpressionA);
+            var expLibForB = CreateExpressionLibraryBlendTree(parameters.ExpressionB);
+
+            var tree = _aac.NewBlendTreeAsRaw();
+            tree.blendType = BlendTreeType.Simple1D;
+            tree.blendParameter = parameters.TweeningAB;
+            tree.useAutomaticThresholds = false;
+            tree.AddChild(expLibForA, 0f);
+            tree.AddChild(expLibForB, 1f);
+
+            return tree;
+        }
+
+        private BlendTree CreateExpressionLibraryBlendTree(string parameter)
+        {
+            var tree = _aac.NewBlendTreeAsRaw();
+            tree.blendType = BlendTreeType.Simple1D;
+            tree.blendParameter = parameter;
+            tree.useAutomaticThresholds = false;
+            for (var index = 0; index < motions.Count; index++)
+            {
+                var motion = motions[index];
+                tree.AddChild(motion, index);
+            }
+
+            return tree;
+        }
+    }
+}
---
+++ Assets/Hai/ComboGestureDynamics/Scripts/Editor/CgdParameters.cs
@@ -0,0 +1,16 @@
+﻿namespace Hai.ComboGestureDynamics.Scripts.Editor
+{
+    public class CgdParameters
+    {
+        private readonly string _prefix;
+
+        public CgdParameters(string prefix)
+        {
+            _prefix = prefix;
+        }
+
+        public string ExpressionA => $"{_prefix}_ExpressionA";
+        public string ExpressionB => $"{_prefix}_ExpressionB";
+        public string TweeningAB => $"{_prefix}_TweeningAB";
+    }
+}
